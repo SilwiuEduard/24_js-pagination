04a547179eb53e58deccef6e2d5eb9f35be21f05
SilwiuEduard <silviu.chiriloaie.it@gmail.com>
2023-05-28 23:30:58 +0200

    added project to Netlify @ README

--- paginate.js
+++ paginate.js
@@ -1,5 +1,5 @@
 const paginate = (followers) => {
-  const itemsPerPage = 8;
+  const itemsPerPage = 10;
   const numberOfPages = Math.ceil(followers.length / itemsPerPage);
 
   const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
